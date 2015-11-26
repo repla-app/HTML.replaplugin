1943625bedcbf421057c1a42657cf05d3c83a652
Roben Kleene <robenk@gmail.com>
2015-11-26 16:50:17 -0500

    Bump ruby gems

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -3,11 +3,10 @@ require 'rbconfig'
 ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-14/2.0.0-static/ffi-1.9.10"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/ffi-1.9.10/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/flt-1.5.0/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-fsevent-0.9.6/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-inotify-0.9.5/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/listen-3.0.5/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/nio-0.2.5/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.1.18/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.1.19/lib"
